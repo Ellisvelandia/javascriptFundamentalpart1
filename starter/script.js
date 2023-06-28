61b557f61bdd2c272be2535add33ef4e2bba9fe3
Ellisvelandia <eyis619l@gmail.com>
2023-06-27 20:31:12 -0500

    javascript fundamental part 1

---
+++ starter/script.js
@@ -0,0 +1,81 @@
+let js = "amazing";
+if (js === "amazing") console.log("Javascript is FUN!");
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("EllisCrisanto");
+console.log(23);
+
+let firstName = "Ellis";
+let firstNamePerson = "Cris";
+
+console.log(firstName);
+console.log(firstNamePerson);
+
+let myFirstJob = "Programer";
+let myCurrentJob = "Teacher";
+
+console.log(`My first job was ${myFirstJob} and now my job is ${myCurrentJob}`);
+
+//Data types
+
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+
+console.log(typeof false);
+console.log(typeof javascriptIsFun);
+console.log(typeof firstName);
+
+let year;
+console.log(year);
+
+year = 1993;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1995;
+
+//Basic Operators
+
+//Math operators
+const now = 2023;
+const ageEllis = now - 1993;
+const ageX = now - 2000;
+console.log(ageEllis, ageX);
+console.log(ageEllis * 2 , ageEllis / 10, 2 *3);
+
+const firstNameEllis  = "Ellis";
+const lastNameEllis = "velandia";
+console.log(firstNameEllis + ' ' + lastNameEllis);
+
+//Assigment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+//Comparison operators
+
+console.log(ageEllis > ageX);
+console.log(ageX >= 18);
+console.log(now - 1991 > now - 2018);
+
+let z , y;
+z = y = 25 - 10 -5;
+console.log(x, y);
+
+const averageAge = (ageEllis + ageX) / 2;
+console.log(ageEllis, ageX, averageAge);
+
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
